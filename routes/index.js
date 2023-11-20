56bae0f55d38580309ab22cb06a8602857719771
hanker145 <phamvangiau2311@gmail.com>
2023-11-20 21:13:45 +0700

    api

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 // localhost:8000/pokemons/
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/api", function (req, res, next) {
   res.status(200).send("Welcome to CoderDex!");
 });
 const pokemonRouter = require("./pokemons.api.js");
-router.use("/pokemons", pokemonRouter);
+router.use("/api/pokemons", pokemonRouter);
 
 module.exports = router;
